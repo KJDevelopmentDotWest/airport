179414b16767b3e2df170463ea8395e53d483bec
bring <bringonthepretty@gmail.com>
2021-09-02 19:58:42 +0300

    upd

--- src/main/java/com/epam/jwd/view/Main.java
+++ src/main/java/com/epam/jwd/view/Main.java
@@ -4,7 +4,6 @@ import com.epam.jwd.repository.storage.Repository;
 
 public class Main {
     public static void main(String[] args) {
-        Repository repository = new Repository();
-        System.out.println(repository.findAllCompanies().get(0).getName());
+
     }
 }
