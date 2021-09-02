bd7e730dc3e4c1377d5187bdc10b3fe5db76cc38
bring <bringonthepretty@gmail.com>
2021-09-02 20:42:36 +0300

    upd

--- src/main/java/com/epam/jwd/service/Service.java
+++ src/main/java/com/epam/jwd/service/Service.java
@@ -1,6 +1,6 @@
 package com.epam.jwd.service;
 
-public class service {
+public class Service {
     public void method(){
         System.out.println("hello world2");
         System.out.println("hello world2");
