7e2c0ea722ee02bfa8d5690ee06ef5c4e9f13de8
bring <bringonthepretty@gmail.com>
2021-09-04 22:59:05 +0300

    upd

--- src/main/java/com/epam/jwd/repository/storage/Repository.java
+++ src/main/java/com/epam/jwd/repository/storage/Repository.java
@@ -19,99 +19,99 @@ public class Repository {
 
     private static final List<Airplane> defaultAirplanes = new ArrayList<>();
 
-//    static {
-//        companies.add(new Company(
-//                "CompanyOne",
-//                Arrays.asList(
-//                        new PassengerPlane.Builder()
-//                                .withFuelConsumption(10)
-//                                .withManufacturer("Boeing")
-//                                .withModel("777")
-//                                .withRange(1000)
-//                                .withSeats(400)
-//                                .withId(1)
-//                                .build(),
-//                        new PassengerPlane.Builder()
-//                                .withFuelConsumption(12)
-//                                .withManufacturer("Boeing")
-//                                .withModel("787")
-//                                .withRange(1200)
-//                                .withSeats(500)
-//                                .withId(2)
-//                                .build(),
-//                        new CargoPlane.Builder()
-//                                .withFuelConsumption(20)
-//                                .withManufacturer("Boeing")
-//                                .withModel("767")
-//                                .withRange(1900)
-//                                .withPayload(4000)
-//                                .withId(3)
-//                                .build(),
-//                        new CargoPlane.Builder()
-//                                .withFuelConsumption(40)
-//                                .withManufacturer("Airbus")
-//                                .withModel("A300")
-//                                .withRange(4000)
-//                                .withPayload(8000)
-//                                .withId(4)
-//                                .build()
-//                )
-//        ));
-//        companies.add(new Company(
-//                "CompanyTwo",
-//                Arrays.asList(
-//                        new PassengerPlane.Builder()
-//                                .withFuelConsumption(10)
-//                                .withManufacturer("Boeing")
-//                                .withModel("777")
-//                                .withRange(1000)
-//                                .withSeats(400)
-//                                .withId(1)
-//                                .build(),
-//                        new PassengerPlane.Builder()
-//                                .withFuelConsumption(12)
-//                                .withManufacturer("Boeing")
-//                                .withModel("787")
-//                                .withRange(1200)
-//                                .withSeats(500)
-//                                .withId(2)
-//                                .build(),
-//                        new CargoPlane.Builder()
-//                                .withFuelConsumption(20)
-//                                .withManufacturer("Boeing")
-//                                .withModel("767")
-//                                .withRange(1900)
-//                                .withPayload(4000)
-//                                .withId(3)
-//                                .build(),
-//                        new CargoPlane.Builder()
-//                                .withFuelConsumption(40)
-//                                .withManufacturer("Airbus")
-//                                .withModel("A300")
-//                                .withRange(4000)
-//                                .withPayload(8000)
-//                                .withId(4)
-//                                .build()
-//                )
-//        ));
-//    }
-
     static {
-        try {
-            FileInputStream fis = new FileInputStream("src/main/resources/data.txt");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            Object object = ois.readObject();
-            if (object instanceof List){
-                companies.addAll((List<Company>) object);
-            }
-        } catch (FileNotFoundException e) {
-            logger.error("FileNotFoundException happened", e);
-        } catch (IOException e) {
-            logger.error("IOException happened", e);
-        } catch (ClassNotFoundException e) {
-            logger.error("ClassNotFoundException happened", e);
-        }
+        companies.add(new Company(
+                "CompanyOne",
+                Arrays.asList(
+                        new PassengerPlane.Builder()
+                                .withFuelConsumption(10)
+                                .withManufacturer("Boeing")
+                                .withModel("777")
+                                .withRange(1000)
+                                .withSeats(400)
+                                .withId(1)
+                                .build(),
+                        new PassengerPlane.Builder()
+                                .withFuelConsumption(12)
+                                .withManufacturer("Boeing")
+                                .withModel("787")
+                                .withRange(1200)
+                                .withSeats(500)
+                                .withId(2)
+                                .build(),
+                        new CargoPlane.Builder()
+                                .withFuelConsumption(20)
+                                .withManufacturer("Boeing")
+                                .withModel("767")
+                                .withRange(1900)
+                                .withPayload(4000)
+                                .withId(3)
+                                .build(),
+                        new CargoPlane.Builder()
+                                .withFuelConsumption(40)
+                                .withManufacturer("Airbus")
+                                .withModel("A300")
+                                .withRange(4000)
+                                .withPayload(8000)
+                                .withId(4)
+                                .build()
+                )
+        ));
+        companies.add(new Company(
+                "CompanyTwo",
+                Arrays.asList(
+                        new PassengerPlane.Builder()
+                                .withFuelConsumption(10)
+                                .withManufacturer("Boeing")
+                                .withModel("777")
+                                .withRange(1000)
+                                .withSeats(400)
+                                .withId(1)
+                                .build(),
+                        new PassengerPlane.Builder()
+                                .withFuelConsumption(12)
+                                .withManufacturer("Boeing")
+                                .withModel("787")
+                                .withRange(1200)
+                                .withSeats(500)
+                                .withId(2)
+                                .build(),
+                        new CargoPlane.Builder()
+                                .withFuelConsumption(20)
+                                .withManufacturer("Boeing")
+                                .withModel("767")
+                                .withRange(1900)
+                                .withPayload(4000)
+                                .withId(3)
+                                .build(),
+                        new CargoPlane.Builder()
+                                .withFuelConsumption(40)
+                                .withManufacturer("Airbus")
+                                .withModel("A300")
+                                .withRange(4000)
+                                .withPayload(8000)
+                                .withId(4)
+                                .build()
+                )
+        ));
     }
+
+//    static {
+//        try {
+//            FileInputStream fis = new FileInputStream("src/main/resources/data.txt");
+//            ObjectInputStream ois = new ObjectInputStream(fis);
+//            Object object = ois.readObject();
+//            if (object instanceof List){
+//                companies.addAll((List<Company>) object);
+//            }
+//        } catch (FileNotFoundException e) {
+//            logger.error("FileNotFoundException happened", e);
+//        } catch (IOException e) {
+//            logger.error("IOException happened", e);
+//        } catch (ClassNotFoundException e) {
+//            logger.error("ClassNotFoundException happened", e);
+//        }
+//    }
 
     public static List<Airplane> getDefaultAirplanes(){
         return defaultAirplanes;
