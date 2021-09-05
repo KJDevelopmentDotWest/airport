14bdaef2add4031939e0bb0eb4a42287763b1072
Fedor Manko <tfma94@gmail.com>
2021-09-05 18:48:59 +0300

    Add new methods in Controller

--- src/main/java/com/epam/jwd/view/Main.java
+++ src/main/java/com/epam/jwd/view/Main.java
@@ -1,22 +1,31 @@
 package com.epam.jwd.view;
 
+import com.epam.jwd.controller.Controller;
+import com.epam.jwd.service.editor.CompanyEditor;
+import com.epam.jwd.service.exeption.MinMaxFuelConsumptionExeption;
 import com.epam.jwd.service.exeption.WrongIdException;
+import com.epam.jwd.service.reader.RepositoryReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     private static final String MAIN_METHOD_MESSAGE = "We're in main method";
 
-    public static void main(String[] args) throws WrongIdException {
-//        logger.debug(MAIN_METHOD_MESSAGE);
-//        CompanyEditor companyEditor = new CompanyEditor(Controller.createCompany("company3"));
-//        companyEditor.addAirplaneToCompany(RepositoryReader.getDefaultAirplanes().get(0).getId());
-//        System.out.println(Controller.getCompanies().get(2).getAirplanes());
-//        //System.out.println(Controller.getCompanies().get(0).getAirplanes());
-//        View view = new View();
-//        view.start;
+    public static void main(String[] args) throws WrongIdException, MinMaxFuelConsumptionExeption {
+        logger.debug(MAIN_METHOD_MESSAGE);
+        CompanyEditor companyEditor = new CompanyEditor(Controller.createCompany("company3"));
+        companyEditor.addAirplaneToCompany(RepositoryReader.getDefaultAirplanes().get(0).getId());
+        System.out.println(Controller.getCompanies().get(2).getAirplanes());
+        System.out.println(Controller.calculateTotalCapacity(RepositoryReader.getCompanies().get(1)));
+        System.out.println(Controller.calculatePayload(RepositoryReader.getCompanies().get(0)));
+        System.out.println(Controller.sortByRange(RepositoryReader.getCompanies().get(0)));
+        System.out.println(Controller.sortByFuelConsumptionRange(RepositoryReader.getCompanies().get(0)));
+        System.out.println(Controller.getCompany(RepositoryReader.getCompanies().get(2)));
+
     }
 
 }
